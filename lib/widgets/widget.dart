2e49662158a2271893d24ab92b73c57ab7245856
glowisn <asdxcv1998@gmail.com>
2022-01-11 21:08:05 +0900

    Update widget.dart

--- lib/widgets/widget.dart
+++ lib/widgets/widget.dart
@@ -1,31 +1,34 @@
 import 'package:do_it_church/constants/colors.dart';
 import 'package:flutter/material.dart';
-//radio widget
-Widget RadioWidget(int? isSelected, var controllerValue, List<String> choice, TextStyle textStyle) {
-  return StatefulBuilder(
-              builder: (BuildContext context, StateSetter setState) {
-            return Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                for(int i = 0; i < choice.length ; i++)
-                Row(
-                  children: [
-                    Radio<int>(
-                      value: i + 1,
-                      groupValue: isSelected,
-                      onChanged: (int? value) {
-                        setState(() => isSelected = value);
-                        controllerValue = choice[i];
-                      },
-                    ),
-                    Text(choice[i], style: textStyle,),
-                  ],
-                ),
-              ],
-            );
-          });
-} 
 
+//radio widget
+Widget RadioWidget(int? isSelected, var controllerValue, List<String> choice,
+    TextStyle textStyle) {
+  return StatefulBuilder(builder: (BuildContext context, StateSetter setState) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        for (int i = 0; i < choice.length; i++)
+          Row(
+            children: [
+              Radio<int>(
+                value: i + 1,
+                groupValue: isSelected,
+                onChanged: (int? value) {
+                  setState(() => isSelected = value);
+                  controllerValue = choice[i];
+                },
+              ),
+              Text(
+                choice[i],
+                style: textStyle,
+              ),
+            ],
+          ),
+      ],
+    );
+  });
+}
 
 ElevatedButton RoundButtonWhiteBackground(
   VoidCallback callback,
@@ -74,15 +77,16 @@ ElevatedButton RoundButtonColorBackground(
 
 //textfield
 TextField textField(controller, hint) {
-return TextField(
-  controller: controller,
-  decoration: InputDecoration(
-    border: OutlineInputBorder(
-      borderRadius: BorderRadius.all(Radius.circular(50)),
+  return TextField(
+    controller: controller,
+    decoration: InputDecoration(
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(50)),
+      ),
+      fillColor: Color(0xFFF2F4F6),
+      filled: true,
+      labelText: hint,
+      //labelStyle: TextStyle()
     ),
-    fillColor: Color(0xFFF2F4F6),
-    filled: true,
-    labelText: hint,
-    //labelStyle: TextStyle()
-  ),
-);
+  );
+}
