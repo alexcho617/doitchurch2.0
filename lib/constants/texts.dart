008f71e1013bfda1ea33f093a20251c4a173b726
Alex Cho <alexcho617@gmail.com>
2022-01-11 21:29:31 +0900

    Changed Themes

--- lib/constants/texts.dart
+++ lib/constants/texts.dart
@@ -1,4 +1,12 @@
+import 'package:do_it_church/constants/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-
+TextTheme kBasicTextTheme = TextTheme(
+  bodyText2: TextStyle(
+    fontFamily: 'Rubik',
+    fontSize: 16.sp,
+    letterSpacing: 1,
+    color: PrimaryTextColor,
+  ),
+);
--- lib/constants/themes.dart
+++ lib/constants/themes.dart
@@ -1,32 +1,25 @@
 import 'package:do_it_church/constants/colors.dart';
+import 'package:do_it_church/constants/texts.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 ThemeData BasicTheme = ThemeData(
-  scaffoldBackgroundColor: Colors.white,
-  primaryColor: PrimaryColor,
-  iconTheme: IconThemeData(color: PrimaryColor),
-  outlinedButtonTheme: OutlinedButtonThemeData(
-    style: OutlinedButton.styleFrom(
-      side: BorderSide(color: PrimaryColor),
-      primary: PrimaryColor,
-    ),
-  ),
-  textButtonTheme: TextButtonThemeData(
-    style: TextButton.styleFrom(
-      primary: Color(0xFF687AB5),
-    ),
-  ),
-  appBarTheme: AppBarTheme(
+    scaffoldBackgroundColor: Colors.white,
+    primaryColor: PrimaryColor,
     iconTheme: IconThemeData(color: PrimaryColor),
-    backgroundColor: Colors.white,
-  ),
-  textTheme: TextTheme(
-    bodyText2: TextStyle(
-      fontFamily: 'Rubik',
-      fontSize: 16.sp,
-      letterSpacing: 1,
-      color: PrimaryColor,
+    outlinedButtonTheme: OutlinedButtonThemeData(
+      style: OutlinedButton.styleFrom(
+        side: BorderSide(color: PrimaryColor),
+        primary: PrimaryColor,
+      ),
     ),
-  ),
-);
+    textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(
+        primary: Color(0xFF687AB5),
+      ),
+    ),
+    appBarTheme: AppBarTheme(
+      iconTheme: IconThemeData(color: PrimaryColor),
+      backgroundColor: Colors.white,
+    ),
+    textTheme: kBasicTextTheme);
